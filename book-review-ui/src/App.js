5ace2fbb7f9f957112926f936cffd9b7191b153e
Rafal Gostynski <rgostynski@gmail.com>
2022-07-04 19:47:59 +1200

    Author component start

--- book-review-ui/src/App.js
+++ book-review-ui/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Book from './components/Book'
+import Author from './components/Author';
 
 function App() {
   return (
     <div className="App">
       <Book />
+      <Author />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ book-review-ui/src/components/Author.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class Author extends React.Component {
+
+    render() {
+        return(<p>The Author info will go here</p>)
+    }
+}
+
+export default Author
