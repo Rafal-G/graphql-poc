e900db3cea2e761dec4657aff6e7354204d43669
Rafal Gostynski <rgostynski@gmail.com>
2022-07-05 19:19:43 +1200

    Two new routes, also some small cleanup

--- book-review-ui/src/App.js
+++ book-review-ui/src/App.js
@@ -1,27 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
-import Book from './components/Book'
-import Author from './components/Author';
+import { Link } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Book />
-      <Author />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+        <Link to="/books">Books</Link> |{" "}
+        <Link to="/authors">Authors</Link>
+      </nav>
     </div>
   );
 }
