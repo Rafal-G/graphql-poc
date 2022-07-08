8dfa99f559544048b77a3804eb03e1e3ca53c862
Rafal Gostynski <rgostynski@gmail.com>
2022-07-08 20:54:29 +1200

    Getting authors

--- book-review-ui/src/routes/Authors.js
+++ book-review-ui/src/routes/Authors.js
@@ -1,12 +1,40 @@
 import React from 'react';
 import TopNav from '../components/TopNav';
+import {useQuery, gql} from '@apollo/client'
 
+const GET_AUTHORS = gql`
+    query {
+    authors {
+        id,
+        name,
+        books {
+        id,
+        name
+        }
+    }
+    }
+`
+function GetAuthors() {
+    const { loading, error, data } = useQuery(GET_AUTHORS);
+    if (loading) return <p>Loading...</p>;
+
+    if (error) return <p>Error :(</p>;
+    data.authors.map((author) => {
+        console.log('author', author)
+    })
+
+    return data.authors.map(({id, name}) => {
+        <div key={id}>        
+            <p>{id} {name}</p>
+        </div>
+    })
+}
 class Authors extends React.Component {
-
     render() {
         return (
         <div>
             <TopNav></TopNav>
+            <GetAuthors></GetAuthors>
             <p>The Authors info will go here</p>
         </div>
 
