793cc6e92fd9f6340f5c6f338e25fd8fa8b11c54
Rafal Gostynski <rgostynski@gmail.com>
2022-07-12 15:46:22 +1200

    Loading author detail from route

--- book-review-ui/src/routes/Author.js
+++ book-review-ui/src/routes/Author.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 
+function AuthorDetail() {
+    let params = useParams();
+
+    return (
+    <div>
+        <p>Single Author: {params.authorId}</p>
+    </div>
+    )
+}
+
 class Author extends React.Component {
     render() {
-        let params = useParams();
-
-        return (
-        <div>
-            <p>Single Author: {params.authorId}</p>
-        </div>
+        
+        return(
+            <div>
+                <AuthorDetail />
+            </div>
         )
     }
 }
